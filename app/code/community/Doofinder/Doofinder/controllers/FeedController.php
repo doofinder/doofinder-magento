42a2bb1807369780294b1d8081f98e9af22c56b7
Carlos Escribano Rey <carlos@markhaus.com>
2013-05-13 14:14:32 +0200

    Added new controller for the future configuration wizard from the Doofinder admin pane.
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- app/code/community/Doofinder/Doofinder/controllers/FeedController.php
+++ app/code/community/Doofinder/Doofinder/controllers/FeedController.php
@@ -5,19 +5,28 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
   const CATEGORY_SEPARATOR = '/';
   const CATEGORY_TREE_SEPARATOR = '>';
 
-  private static $csvHeader = array('id', 'title', 'link', 'description', 'price', 'sale_price', 'image_link', 'categories', 'availability', 'brand', 'gtin', 'mpn', 'extra_title_1', 'extra_title_2');
-
+  private static $csvHeader = array('id', 'title', 'link', 'description',
+                                    'price', 'sale_price', 'image_link',
+                                    'categories', 'availability', 'brand',
+                                    'gtin', 'mpn', 'extra_title_1',
+                                    'extra_title_2');
   private $_aCategories;
   private $_iCurrentStoreId;
   private $_oCurrentStore;
   private $_oTaxHelper;
 
-  // (/default)?/doofinder/feed/version
+  /**
+   * Outputs the version number of the module in plain text.
+   *
+   * URL: (/default)?/doofinder/feed/version
+   */
   public function versionAction()
   {
     $request = $this->getRequest();
     $app = Mage::app();
-    $app->loadAreaPart(Mage_Core_Model_App_Area::AREA_FRONTEND, Mage_Core_Model_App_Area::PART_EVENTS);
+    $app->loadAreaPart(
+      Mage_Core_Model_App_Area::AREA_FRONTEND,
+      Mage_Core_Model_App_Area::PART_EVENTS);
 
     $this->getResponse()
       ->clearHeaders()
@@ -27,6 +36,60 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     die(Mage::getConfig()->getNode()->modules->Doofinder_Doofinder->version);
   }
 
+  /**
+   * Outputs a JSON string describing the Magento installation and the Doofinder
+   * module configuration.
+   *
+   * URL: (/default)?/doofinder/feed/config
+   */
+  public function configAction()
+  {
+    $request = $this->getRequest();
+    $app = Mage::app();
+    $app->loadAreaPart(
+      Mage_Core_Model_App_Area::AREA_FRONTEND,
+      Mage_Core_Model_App_Area::PART_EVENTS);
+
+    $this->getResponse()
+      ->clearHeaders()
+      ->setHeader('Content-Type','application/json; charset=UTF-8')
+      ->sendHeaders();
+
+    $oConfig = Mage::getConfig()->getNode();
+
+    $feeds = array();
+
+    foreach ($app->getStores() as $store)
+    {
+      $code = $store->getCode();
+      $feeds[] = array(
+        'code' => $code,
+        'url' => $store->getUrl('doofinder/feed')
+      );
+    }
+
+    $cfg = array(
+      "platform" => array(
+        "name" => "Magento",
+        "version" => Mage::getVersion()
+      ),
+      "module" => array(
+        "version" => $oConfig->modules->Doofinder_Doofinder->version,
+        "feeds" => $feeds
+      )
+    );
+
+    echo json_encode($cfg);
+    exit();
+  }
+
+  /**
+   * Returns a collection of (int) $limit products from (int) $offset.
+   *
+   * @param int Offset (Default: 0)
+   * @param int Limit (Default: 0 - Unlimited)
+   * @return Collection products
+   */
   private function _getProductCollection($offset = 0, $limit = 0)
   {
     $collection = Mage::getModel('catalog/product')
@@ -52,8 +115,11 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     return $collection;
   }
 
-
-
+  /**
+   * Returns the deepest categories paths for a product.
+   * @param object Product
+   * @return array of strings.
+   */
   private function _getCategoriesFor($oProduct)
   {
     if ($this->_aCategories === null)
@@ -104,12 +170,19 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     return $result;
   }
 
+  /**
+   * Returns the number of products of the data feed.
+   * @return integer
+   */
   private function _countProducts()
   {
     $collection = $this->_getProductCollection();
     return $collection->getSize();
   }
 
+  /**
+   * Outputs a product of the feed based on current configuration.
+   */
   public function _addProductToFeed($args)
   {
     $row = $args['row'];
@@ -119,6 +192,14 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     $product->setData($row);
     $product->setStoreId($this->_iCurrentStoreId);
     $product->getResource()->load($product, $row['entity_id']);
+
+    $actualPrice = $this->_oTaxHelper->getPrice($product,
+                                                $product->getPrice(), true);
+    $salePrice = $this->_oTaxHelper->getPrice($product,
+                                              $product->getFinalPrice(), true);
+
+    if ($actualPrice <= 0 && $salePrice <= 0)
+      return;
 
     // ID
     echo $product->getId().self::TXT_SEPARATOR;
@@ -133,16 +214,16 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     // DESCRIPTION
     echo self::cleanString($product->getDescription()).self::TXT_SEPARATOR;
 
-    $actualPrice = $this->_oTaxHelper->getPrice($product, $product->getPrice(), true);
-    $specialPrice = $this->_oTaxHelper->getPrice($product, $product->getFinalPrice(), true);
-
     // PRICE
+    if ($actualPrice <= 0)
+      $actualPrice = $salePrice;
+
     echo number_format(self::cleanString($actualPrice), 2, '.', '').self::TXT_SEPARATOR;
 
     // SALE PRICE
-    if ($actualPrice > $specialPrice)
+    if ($actualPrice > $salePrice)
     {
-      echo number_format(self::cleanString($specialPrice), 2, '.', '').self::TXT_SEPARATOR;
+      echo number_format(self::cleanString($salePrice), 2, '.', '').self::TXT_SEPARATOR;
     }
     else
     {
@@ -177,12 +258,20 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     flush(); ob_flush();
   }
 
+  /**
+   * Get the store code from $_GET and init the store in the object instance.
+   */
   private function _initStore()
   {
     $app = Mage::app();
-    $app->loadAreaPart(Mage_Core_Model_App_Area::AREA_FRONTEND, Mage_Core_Model_App_Area::PART_EVENTS);
+    $app->loadAreaPart(
+      Mage_Core_Model_App_Area::AREA_FRONTEND,
+      Mage_Core_Model_App_Area::PART_EVENTS);
 
-    $store = $app->getSafeStore($this->getRequest()->getParam('store', null));
+    if (! ($store = $this->getRequest()->getParam('___store', null)))
+      $store = $this->getRequest()->getParam('store', null);
+
+    $store = $app->getSafeStore($store);
 
     if (!$store || !$store->getCode() || !$store->getIsActive())
       return false;
@@ -195,8 +284,32 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     return true;
   }
 
-  // (/default)?/doofinder/feed/
-  // (/default)?/doofinder/feed/index
+  /**
+   * Index Action
+   *
+   * Controller to output the data feed in text/plain format.
+   *
+   * @param integer $_GET limit Optional. If present the action will only output
+   *                            a limited number of results starting from the
+   *                            value of the "offset" parameter (defaults to 0).
+   * @param integer $_GET offset Optional. Used only with "limit".
+   * @param integer $_GET chunksize Size of the results block got from database
+   *                                on each query.
+   * @param string $_GET store Optional. Defaults to the default store. Code of
+   *                           the store we want to get the data from.
+   * @param string $_GET ___store Alias of "store".
+   *
+   * Sample URLs:
+   *
+   * (/<store>)?/doofinder/feed/index
+   * (/<store>)?/doofinder/feed/
+   * (/<store>)?/doofinder/feed?chunksize=1000
+   * (/<store>)?/doofinder/feed?offset=0&limit=10
+   * (/<store>)?/doofinder/feed?limit=10
+   * /doofinder/feed?___store=english
+   * /doofinder/feed?store=spanish
+   *
+   */
   public function indexAction()
   {
     $request = $this->getRequest();
@@ -223,26 +336,52 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
 
     // Send products
 
-    echo implode(self::TXT_SEPARATOR, self::$csvHeader).PHP_EOL;
-    flush(); ob_flush();
+    $iLimit = intval($this->getRequest()->getParam('limit', 0));
+    $iChunk = intval($this->getRequest()->getParam('chunksize', 1000));
+    $iOffset0 = intval($this->getRequest()->getParam('offset', 0));
 
-    $iTotal = $this->_countProducts();
-    $iChunk = 1000;
-
-    for ($iOffset = 0; $iOffset < $iTotal; $iOffset += $iChunk)
+    // (/default)?/doofinder/feed?offset=0&limit=10
+    // (/default)?/doofinder/feed?limit=10
+    if ($iLimit)
     {
-      $iLimit = min($iTotal - $iOffset, $iChunk);
-      $collection = $this->_getProductCollection($iOffset, $iLimit);
+      echo implode(self::TXT_SEPARATOR, self::$csvHeader).PHP_EOL;
+      flush(); ob_flush();
 
+      $collection = $this->_getProductCollection($iOffset0, $iLimit);
       Mage::getSingleton('core/resource_iterator')->walk(
         $collection->getSelect(),
         array(array($this, '_addProductToFeed'))
       );
     }
+    // (/default)?/doofinder/feed?chunksize=1000
+    // (/default)?/doofinder/feed
+    else
+    {
+      $iTotal = $this->_countProducts();
 
-    die();
+      echo implode(self::TXT_SEPARATOR, self::$csvHeader).PHP_EOL;
+      flush(); ob_flush();
+
+      for ($iOffset = 0; $iOffset < $iTotal; $iOffset += $iChunk)
+      {
+        $iLimit = min($iTotal - $iOffset, $iChunk);
+        $collection = $this->_getProductCollection($iOffset, $iLimit);
+
+        Mage::getSingleton('core/resource_iterator')->walk(
+          $collection->getSelect(),
+          array(array($this, '_addProductToFeed'))
+        );
+      }
+    }
+
+    exit();
   }
 
+  /**
+   * Cleans the string passed as parameter to use it in the data feed.
+   * @param string
+   * @return string
+   */
   private static function cleanString($text)
   {
     $text = strip_tags(html_entity_decode($text, ENT_QUOTES, 'UTF-8'));
@@ -257,7 +396,13 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
    * titles that contains references that can be searched with or without
    * certain characters.
    *
+   * Ex: TN-2000 => TN2000
+   *
    * TODO: Make it configurable from the admin.
+   *
+   * @param string
+   * @param string Optional replacement string
+   * @return string
    */
   private static function cleanReferences($text, $repl = "")
   {
@@ -265,6 +410,14 @@ class Doofinder_Doofinder_FeedController extends Mage_Core_Controller_Front_Acti
     return str_replace($forbidden, $repl, $text);
   }
 
+  /**
+   * Separates alphabetical characters from numeric ones by adding a blank.
+   *
+   * Ex: TN2000 => TN 2000
+   *
+   * @param string
+   * @return string
+   */
   private static function splitReferences($text)
   {
     $s = preg_replace("/([^\d\s])([\d])/", "$1 $2", $text);
