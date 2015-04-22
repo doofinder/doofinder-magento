b1f213f910475e740ea67fb097dcc79e0a4c1861
Carlos Escribano <carlos@doofinder.com>
2015-04-22 16:20:27 +0200

    Bugfixes
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Model/Generator.php
+++ app/code/community/Doofinder/Feed/Model/Generator.php
@@ -6,13 +6,13 @@
 /**
  * @category   Models
  * @package    Doofinder_Feed
- * @version    1.5.0
+ * @version    1.5.1
  */
 
 /**
  * Generator model for Doofinder Feed
  *
- * @version    1.5.0
+ * @version    1.5.1
  * @package    Doofinder_Feed
  */
 if (!defined('DS'))
