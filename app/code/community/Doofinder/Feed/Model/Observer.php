f47a671cd51357cf6b99ed5112d87ab9d81693c1
Maciek Lewkowicz <hello@magently.com>
2015-05-21 20:44:32 +0200

    Generate full feed via cron

--- app/code/community/Doofinder/Feed/Model/Observer.php
+++ app/code/community/Doofinder/Feed/Model/Observer.php
@@ -2,8 +2,26 @@
 
 class Doofinder_Feed_Model_Observer
 {
+
+    protected $xmlPath = 'feed.xml';
+
     public function generateFeed()
     {
-        Mage::log('Generating feed');
+
+        $options = array(
+            '_limit_' => null,
+            '_offset_' => 0,
+            'store_code' => 'default',
+            'grouped' => true,
+            // Calculate the minimal price with the tier prices
+            'minimal_price' => false,
+            // Not logged in by default
+            'customer_group_id' => 0,
+        );
+
+
+        $generator = Mage::getSingleton('doofinder_feed/generator', $options);
+        $xmlData = $generator->run();
+        file_put_contents(Mage::getBaseDir('media').DS.'doofinder'.DS.$this->xmlPath, $xmlData);
     }
 }
