3cb207062709580fc72002eaeac64d7ae48e9e51
Bartosz Herba <hello@magently.com>
2015-05-26 19:19:45 +0200

    Connect backend options with observator

--- app/code/community/Doofinder/Feed/Model/Observer.php
+++ app/code/community/Doofinder/Feed/Model/Observer.php
@@ -2,28 +2,123 @@
 
 class Doofinder_Feed_Model_Observer
 {
+    /**
+     * Xml file name.
+     * @var string
+     */
+    protected $xmlPath;
 
-    protected $xmlPath = 'feed.xml';
+    /**
+     * Crontab enabled settings.
+     * @var bool
+     */
+    protected $enabled;
+
+    /**
+     * Crontab grouped settings.
+     * @var bool
+     */
+    protected $grouped;
+
+    /**
+     * Crontab minimial price settings.
+     * @var bool
+     */
+    protected $price;
+
+    /**
+     * Magento store code.
+     * @var string
+     */
+    protected $storeCode;
+
+
+    public function __construct() {
+        $this->enabled = Mage::getStoreConfig('doofinder_cron/settings/enabled', Mage::app()->getStore());
+        $this->price = Mage::getStoreConfig('doofinder_cron/settings/minimal_price', Mage::app()->getStore());
+        $this->grouped = Mage::getStoreConfig('doofinder_cron/settings/grouped', Mage::app()->getStore());
+        $this->storeCode = Mage::app()->getStore()->getCode();
+        $this->xmlPath = Mage::getStoreConfig('doofinder_cron/settings/name', Mage::app()->getStore());
+    }
 
     public function generateFeed()
     {
 
-        $stepSize = 5;
-        $lastRun = 0;
+        if ($this->enabled) {
+            try {
 
-        $options = array(
-            '_limit_' => $stepSize,
-            '_offset_' => $lastRun,
-            'store_code' => 'default',
-            'grouped' => true,
-            // Calculate the minimal price with the tier prices
-            'minimal_price' => false,
-            // Not logged in by default
-            'customer_group_id' => 0,
-        );
+                $stepSize = 5;
+                $lastRun = 0;
 
-        $generator = Mage::getSingleton('doofinder_feed/generator', $options);
-        $xmlData = $generator->run($options);
-        file_put_contents(Mage::getBaseDir('media').DS.'doofinder'.DS.$this->xmlPath, $xmlData);
+                $options = array(
+                    '_limit_' => $stepSize,
+                    '_offset_' => $lastRun,
+                    'store_code' => $this->storeCode,
+                    'grouped' => $this->_getBoolean($this->grouped),
+                    // Calculate the minimal price with the tier prices
+                    'minimal_price' => $this->_getBoolean($this->price),
+                    // Not logged in by default
+                    'customer_group_id' => 0,
+                );
+
+                $generator = Mage::getSingleton('doofinder_feed/generator', $options);
+                $xmlData = $generator->run($options);
+                $dir = Mage::getBaseDir('media').DS.'doofinder';
+                $path = Mage::getBaseDir('media').DS.'doofinder'.DS.$this->xmlPath;
+
+                if (!file_exists($dir)) {
+                    $this->_createDirectory($dir);
+                }
+
+                if (!$success = file_put_contents($path, $xmlData)) {
+                    throw new Exception("File could not be saved: {$path}");
+                }
+            } catch (Exception $e) {
+                Mage::log('Exception: '.$e);
+            }
+        }
+    }
+
+    /**
+     * Creates directory.
+     * @param string $dir
+     * @return bool
+     */
+    protected function _createDirectory($dir = null) {
+        if (!$dir) return false;
+
+        if(!mkdir($dir, 0777, true)) {
+            throw new Exception('Could not create directory: '.$dir);
+        }
+
+        return true;
+    }
+
+    /**
+     * Cast any value to bool
+     * @param mixed $value
+     * @param bool $defaultValue
+     * @return bool
+     */
+    protected function _getBoolean($value, $defaultValue = false)
+    {
+        if (is_numeric($value)) {
+            if ($value)
+                return true;
+            else
+                return false;
+
+        }
+
+        $yes = array('true', 'on', 'yes');
+        $no  = array('false', 'off', 'no');
+
+        if ( in_array($value, $yes) )
+            return true;
+
+        if ( in_array($value, $no) )
+            return false;
+
+        return $defaultValue;
     }
 }
