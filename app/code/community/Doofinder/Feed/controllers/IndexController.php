10d3ae9961cdb3e91d2e1c09dbb6f8eb9465d73f
Bartosz Herba <hello@magently.com>
2015-06-11 10:29:04 +0200

    Fix logic error upon regeneratin schedule Add enabled/disabled logic based on setting upon feed generate

--- app/code/community/Doofinder/Feed/controllers/IndexController.php
+++ app/code/community/Doofinder/Feed/controllers/IndexController.php
@@ -21,4 +21,20 @@ class Doofinder_Feed_IndexController extends Mage_Core_Controller_Front_Action
     {
         $this->_redirect('/');
     }
+
+    public function testAction() {
+        $time = array(12,0,0);
+        $timeStr = strtotime('12:0:0');
+        $month = 0;
+        $day = 0;
+        var_dump(date('Z'));
+
+        var_dump(date('H-i-s'));
+
+
+        $timescheduled = strftime("%Y-%m-%d %H:%M:%S", mktime($time[0], $time[1], $time[2], date("m") + $month, date("d") + $day, date("Y")));
+        $strTime = strtotime('2015-06-11 12:00:00');
+        var_dump(date('Y-m-d H-i-s', $strTime));
+        echo $timescheduled;
+    }
 }
