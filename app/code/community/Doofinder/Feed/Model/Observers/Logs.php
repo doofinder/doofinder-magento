96a4925ec722071393183fa690ed1d4a99bebcd5
Paweł Tatarczuk <hello@magently.com>
2015-08-28 13:27:47 +0200

    Add removing old logs (when log entries count is bigger than 1000)

---
+++ app/code/community/Doofinder/Feed/Model/Observers/Logs.php
@@ -0,0 +1,36 @@
+<?php
+
+class Doofinder_Feed_Model_Observers_Logs
+{
+    const MAX_SIZE = 1000;
+    const BATCH_LIMIT = 100;
+
+    /**
+     * Clear logs that are beyond the limit
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function clearLogs($observer)
+    {
+        $collection = Mage::getModel('doofinder_feed/log')->getCollection();
+
+        $size = $collection->getSize();
+
+        if ($size > static::MAX_SIZE) {
+            $collection->setOrder('id', $collection::SORT_ORDER_DESC);
+
+            $offset = max(static::MAX_SIZE, $size - static::BATCH_LIMIT);
+
+            $collection->getSelect()
+                ->limit(static::BATCH_LIMIT, $offset);
+
+            $ids = array();
+            foreach ($collection->getItems() as $item) {
+                $item->delete();
+                $ids[] = $item->id;
+            }
+
+            Mage::log($ids, null, 'debug.log');
+        }
+    }
+}
