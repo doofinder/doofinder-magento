0773cd825d8bb3dbf0159573b2b2424e1a9b1b4f
Carlos Escribano <carlos@doofinder.com>
2015-10-07 21:06:48 +0200

    Fixed time selector
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- js/doofinder/admin.js
+++ js/doofinder/admin.js
@@ -5,8 +5,8 @@
         try {
             var $td = $('row_doofinder_cron_schedule_settings_time').select('td.value')[0];
             $td.innerHTML = $td.innerHTML.replace(/&nbsp;:&nbsp;/g, '<span class="df-separator"></span>');
-            $td.select('.df-separator:last')[0].hide();
-            $td.select('select:last')[0].hide();
+            $td.select('.df-separator')[1].hide();
+            $td.select('select')[2].hide();
         } catch (e) {}
 
         if ($('doofinder_cron_feed_settings')) {
