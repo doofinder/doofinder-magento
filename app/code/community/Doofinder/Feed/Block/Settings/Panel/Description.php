2971abaeb1fd24e39a80b83e49bc30b4a37ed989
Carlos Escribano <carlos@doofinder.com>
2015-10-08 11:25:05 +0200

    Better message.
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Block/Settings/Panel/Description.php
+++ app/code/community/Doofinder/Feed/Block/Settings/Panel/Description.php
@@ -1,6 +1,7 @@
 <?php
 class Doofinder_Feed_Block_Settings_Panel_Description extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
+    protected $description = 'You can set the rest of the options for each store separately by modifying the Current Configuration Scope.';
 
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
@@ -8,13 +9,20 @@ class Doofinder_Feed_Block_Settings_Panel_Description extends Mage_Adminhtml_Blo
 
         if (!Mage::app()->getRequest()->getParam('store'))
         {
-            $text =  'You can set the rest of the options for each store separately by modifying the Current Configuration Scope.';
+            $text = $this->description;
         }
 
         $this->setElement($element);
         $name = $element->getName();
         $element->setScopeLabel('');
-        $html = '<p class="doofinder-info" style="width: 400px;">' . $text . '</p>';
-        return $html;
+
+        return '<p class="doofinder-info">' . $text . '</p>';
+    }
+
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        $html = '<td class="label"></td>' .
+                '<td class="value" colspan="3">' . $this->_getElementHtml($element) . '</td>';
+        return $this->_decorateRowHtml($element, $html);
     }
 }
---
+++ app/code/community/Doofinder/Feed/Block/Settings/Panel/LayerDescription.php
@@ -0,0 +1,5 @@
+<?php
+class Doofinder_Feed_Block_Settings_Panel_LayerDescription extends Doofinder_Feed_Block_Settings_Panel_Description
+{
+  protected $description = 'The layer script must be configured separately for each store view by modifying the Current Configuration Scope.';
+}
