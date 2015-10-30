b62081e7eb15c1277515f98afb494d6c6a9738e1
Bartosz Herba <hello@magently.com>
2015-10-30 10:32:17 +0100

    Change search enable option scope to global Add has id config information

---
+++ app/code/community/Doofinder/Feed/Block/Settings/Panel/Hashdescription.php
@@ -0,0 +1,5 @@
+<?php
+class Doofinder_Feed_Block_Settings_Panel_HashDescription extends Doofinder_Feed_Block_Settings_Panel_Description
+{
+  protected $description = 'The Hash ID value must be configured separately for each store view by modifying the Current Configuration Scope.';
+}
