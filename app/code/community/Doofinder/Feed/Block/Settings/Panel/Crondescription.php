286645b1c5f57f1adb99a093c4b310a4df404978
Carlos Escribano <carlos@doofinder.com>
2015-12-21 13:17:11 +0100

    small changes
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Block/Settings/Panel/Crondescription.php
+++ app/code/community/Doofinder/Feed/Block/Settings/Panel/Crondescription.php
@@ -11,5 +11,5 @@
 
 class Doofinder_Feed_Block_Settings_Panel_CronDescription extends Doofinder_Feed_Block_Settings_Panel_Description
 {
-  protected $description = 'Feed can be generated directly in your server to save computer resources. See <a href="http://support.doofinder.com/customer/portal/articles/2153665-how-to-install-doofinder-in-magento">this article</a> for more information.';
+  protected $description = 'THIS FEATURE IS CURRENTLY IN BETA.<br>Feeds can be generated directly in your server to save computer resources. See <a href="http://support.doofinder.com/customer/portal/articles/2153665-how-to-install-doofinder-in-magento">this article</a> for more information.';
 }
