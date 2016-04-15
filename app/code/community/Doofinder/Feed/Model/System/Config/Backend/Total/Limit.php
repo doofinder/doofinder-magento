208c41632e3ed701414eb2a1e3fadb01301d93d5
Carlos Escribano <carlos@doofinder.com>
2016-04-15 11:17:50 +0200

    Fixed problem with number detection
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Model/System/Config/Backend/Total/Limit.php
+++ app/code/community/Doofinder/Feed/Model/System/Config/Backend/Total/Limit.php
@@ -6,9 +6,11 @@ class Doofinder_Feed_Model_System_Config_Backend_Total_Limit extends Mage_Core_M
     {
         if (!$this->getValue()) {
             throw new Exception(Mage::helper('doofinder_feed')->__('Total limit is required.'));
-        } else if (!is_int($this->getValue())) {
+        } else if (!is_numeric($this->getValue())) {
             throw new Exception(Mage::helper('doofinder_feed')->__('Total limit is not a number.'));
         }
+
+        $this->setValue(intval($this->getValue()));
 
         return parent::_beforeSave();
     }
