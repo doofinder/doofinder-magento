580f716bc1ba2c066bc99a6a5405dfe924b82ebb
Bartosz Herba <hello@magently.com>
2016-05-17 13:24:58 +0200

    Add hashId validation on save in admin panel. Hashid must have unique value.

---
+++ app/code/community/Doofinder/Feed/Model/Adminhtml/System/Config/Validation/HashId.php
@@ -0,0 +1,19 @@
+<?php
+
+class Doofinder_Feed_Model_Adminhtml_System_Config_Validation_HashId extends Mage_Core_Model_Config_Data {
+    public function save() {
+        // Hash id to save
+        $hashId = $this->getValue();
+        $stores = Mage::app()->getStores();
+
+        foreach ($stores as $store) {
+            $code = $store->getCode();
+            $scopeHashId = Mage::getStoreConfig('doofinder_search/internal_settings/hash_id', $code);
+            if ($hashId === $scopeHashId) {
+                Mage::throwException("HashID ".$hashId." is already used in ".$code." store. It must have a unique value.");
+                exit;
+            }
+        }
+        return parent::save();
+    }
+}
