27544aee060824020314402338fe5b120eed65d3
Bartosz Herba <hello@magently.com>
2016-05-27 15:09:22 +0200

    Update searchIndexes action to ommit disabled search engines and display more accurate warning message if engine is enabled but hashid is an empty string.

--- app/code/community/Doofinder/Feed/Model/Adminhtml/System/Config/Validation/HashId.php
+++ app/code/community/Doofinder/Feed/Model/Adminhtml/System/Config/Validation/HashId.php
@@ -5,15 +5,15 @@ class Doofinder_Feed_Model_Adminhtml_System_Config_Validation_HashId extends Mag
         // Hash id to save
         $hashId = $this->getValue();
         $stores = Mage::app()->getStores();
-
         foreach ($stores as $store) {
             $code = $store->getCode();
             $scopeHashId = Mage::getStoreConfig('doofinder_search/internal_settings/hash_id', $code);
-            if ($hashId === $scopeHashId) {
+            if ($hashId !== '' && $hashId === $scopeHashId) {
                 Mage::throwException("HashID ".$hashId." is already used in ".$code." store. It must have a unique value.");
                 exit;
             }
         }
+
         return parent::save();
     }
 }
