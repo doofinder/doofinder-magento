818cefe8f15a3c56b01a393898bcc79a90a9f6eb
Carlos Escribano <carlos@doofinder.com>
2016-09-20 09:55:34 +0200

    New release.
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Model/Observers/Feed.php
+++ app/code/community/Doofinder/Feed/Model/Observers/Feed.php
@@ -6,7 +6,7 @@
 /**
  * @category   Models
  * @package    Doofinder_Feed
- * @version    1.6.1
+ * @version    1.6.2
  */
 
 require_once(Mage::getBaseDir('lib') . DS. 'Doofinder' . DS .'doofinder_management_api.php');
