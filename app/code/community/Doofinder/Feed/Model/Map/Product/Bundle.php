46628c1ef8a19f324301491b4fd3c540b12607b6
Carlos Escribano <carlos@doofinder.com>
2016-10-28 12:33:22 +0200

    New release.
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Model/Map/Product/Bundle.php
+++ app/code/community/Doofinder/Feed/Model/Map/Product/Bundle.php
@@ -6,13 +6,13 @@
 /**
  * @category   Models
  * @package    Doofinder_Feed
- * @version    1.6.5
+ * @version    1.6.6
  */
 
 /**
  * Bundle Product Map Model for Doofinder Feed
  *
- * @version    1.6.5
+ * @version    1.6.6
  * @package    Doofinder_Feed
  */
 class Doofinder_Feed_Model_Map_Product_Bundle
--- app/code/community/Doofinder/Feed/controllers/FeedController.php
+++ app/code/community/Doofinder/Feed/controllers/FeedController.php
@@ -6,13 +6,13 @@
 /**
  * @category   controllers
  * @package    Doofinder_Feed
- * @version    1.6.5
+ * @version    1.6.6
  */
 
 /**
  * Feed controller for Doofinder Feed
  *
- * @version    1.6.5
+ * @version    1.6.6
  * @package    Doofinder_Feed
  */
 class Doofinder_Feed_FeedController extends Mage_Core_Controller_Front_Action
