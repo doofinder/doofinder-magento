e553feb5a57f91dfe46c315465b643d85ded62e9
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2017-03-28 10:35:42 +0200

    Fix availability field in associated product mapper

--- app/code/community/Doofinder/Feed/Model/Map/Product/Associated.php
+++ app/code/community/Doofinder/Feed/Model/Map/Product/Associated.php
@@ -107,18 +107,17 @@ class Doofinder_Feed_Model_Map_Product_Associated
         return $value;
     }
 
-    public function mapFieldAvailability($params = array())
+    public function mapDirectiveAvailability($params = array())
     {
         $args = array('map' => $params['map']);
-        $value = "";
-        $value = $this->getParentMap()->mapField('availability');
+        $value = $this->getParentMap()->mapDirectiveAvailability($args);
+
         // gets out of stock if parent is out of stock
-        if (strcasecmp($this->getConfig()->getOutOfStockStatus(), $value) == 0)
+        if ($this->getConfig()->getOutOfStockStatus() == $value) {
             return $value;
+        }
 
-        $value = $this->getCellValue($args);
-
-        return $value;
+        return parent::mapDirectiveAvailability($params);
     }
 
     public function mapFieldBrand($params = array())
