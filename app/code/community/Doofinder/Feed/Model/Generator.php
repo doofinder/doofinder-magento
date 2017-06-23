8936ef951d3bdfc00a3ab3f54d5f07de03f63afa
Carlos Escribano <carlos@doofinder.com>
2017-06-23 15:26:06 +0200

    New Release: v1.8.9
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Model/Generator.php
+++ app/code/community/Doofinder/Feed/Model/Generator.php
@@ -6,13 +6,13 @@
 /**
  * @category   Models
  * @package    Doofinder_Feed
- * @version    1.8.8
+ * @version    1.8.9
  */
 
 /**
  * Generator model for Doofinder Feed
  *
- * @version    1.8.8
+ * @version    1.8.9
  * @package    Doofinder_Feed
  */
 if (!defined('DS'))
