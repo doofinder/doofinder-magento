9c62cde5aff3cae45cdf6503109e5af8eab88a6b
Carlos Escribano <carlos@doofinder.com>
2017-07-13 18:03:55 +0200

    New Release: v1.8.13
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Model/System/Config/Source/Product/Attributes.php
+++ app/code/community/Doofinder/Feed/Model/System/Config/Source/Product/Attributes.php
@@ -6,7 +6,7 @@
 /**
  * @category   Models
  * @package    Doofinder_Feed
- * @version    1.8.12
+ * @version    1.8.13
  */
 
 class Doofinder_Feed_Model_System_Config_Source_Product_Attributes
--- app/code/community/Doofinder/Feed/controllers/DoofinderFeedFeedController.php
+++ app/code/community/Doofinder/Feed/controllers/DoofinderFeedFeedController.php
@@ -6,7 +6,7 @@
 /**
  * @category   controllers
  * @package    Doofinder_Feed
- * @version    1.8.12
+ * @version    1.8.13
  */
 
 class Doofinder_Feed_DoofinderFeedFeedController extends Mage_Adminhtml_Controller_Action
