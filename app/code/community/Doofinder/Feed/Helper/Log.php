4b4596744ed6804c4fef1249ea770fc383bc5413
Carlos Escribano <carlos@doofinder.com>
2017-07-31 14:52:26 +0200

    New Release: v1.8.16
    Fixed missing parameter in fulltext search resource.
    
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Helper/Log.php
+++ app/code/community/Doofinder/Feed/Helper/Log.php
@@ -6,13 +6,13 @@
 /**
  * @category   Helpers
  * @package    Doofinder_Feed
- * @version    1.8.15
+ * @version    1.8.16
  */
 
 /**
  * Log helper for Doofinder Feed
  *
- * @version    1.8.15
+ * @version    1.8.16
  * @package    Doofinder_Feed
  */
 class Doofinder_Feed_Helper_Log extends Mage_Core_Helper_Abstract
