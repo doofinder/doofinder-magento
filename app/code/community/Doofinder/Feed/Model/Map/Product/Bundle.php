af23e20afe80deb1ba5aea62054fd5685f6450b0
Carlos Escribano <carlos@doofinder.com>
2017-09-07 09:46:40 +0200

    New Version: v1.8.17
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Model/Map/Product/Bundle.php
+++ app/code/community/Doofinder/Feed/Model/Map/Product/Bundle.php
@@ -6,13 +6,13 @@
 /**
  * @category   Models
  * @package    Doofinder_Feed
- * @version    1.8.16
+ * @version    1.8.17
  */
 
 /**
  * Bundle Product Map Model for Doofinder Feed
  *
- * @version    1.8.16
+ * @version    1.8.17
  * @package    Doofinder_Feed
  */
 class Doofinder_Feed_Model_Map_Product_Bundle
