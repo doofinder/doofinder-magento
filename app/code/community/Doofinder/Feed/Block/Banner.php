89f39b9b21b6c3ab614e1f24c3896d42577a5925
Carlos Escribano <carlos@doofinder.com>
2018-04-03 13:50:24 +0200

    New Release: v1.8.23
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- app/code/community/Doofinder/Feed/Block/Banner.php
+++ app/code/community/Doofinder/Feed/Block/Banner.php
@@ -6,7 +6,7 @@
 /**
  * @category   blocks
  * @package    Doofinder_Feed
- * @version    1.8.22
+ * @version    1.8.23
  */
 
 class Doofinder_Feed_Block_Banner extends Mage_Core_Block_Template
--- app/code/community/Doofinder/Feed/Helper/Log.php
+++ app/code/community/Doofinder/Feed/Helper/Log.php
@@ -6,13 +6,13 @@
 /**
  * @category   Helpers
  * @package    Doofinder_Feed
- * @version    1.8.22
+ * @version    1.8.23
  */
 
 /**
  * Log helper for Doofinder Feed
  *
- * @version    1.8.22
+ * @version    1.8.23
  * @package    Doofinder_Feed
  */
 class Doofinder_Feed_Helper_Log extends Mage_Core_Helper_Abstract
