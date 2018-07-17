69dfc11132d82075fa1e20f1465cb683dd7de063
Adrian Szulc <adrian.szulc@magently.com>
2018-07-17 21:27:48 +0200

    Enforce lowercase and no whitespaces in "field" in additional attributes
    refs #60491
    
    Change-Id: I0643e2e51ac729c96d56bb2a28dbb641e26080aa

--- app/code/community/Doofinder/Feed/Model/System/Config/Backend/Map/Additional.php
+++ app/code/community/Doofinder/Feed/Model/System/Config/Backend/Map/Additional.php
@@ -18,6 +18,14 @@ class Doofinder_Feed_Model_System_Config_Backend_Map_Additional
 
         unset($_value['additional_mapping'][-1]);
 
+        // Prepare data before save
+        // transform 'field' to lowercase
+        // and replace whitespaces with underscore
+        foreach ($_value['additional_mapping'] as $key => $item) {
+            $item['field'] = str_replace(' ', '_', strtolower($item['field']));
+            $_value['additional_mapping'][$key] = $item;
+        }
+
         $this->setValue($_value);
 
         parent::_beforeSave();
