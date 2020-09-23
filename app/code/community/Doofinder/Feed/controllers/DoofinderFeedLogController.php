343378800f427aa60d183f752e2b48b009bf9c46
Carlos Escribano <carlos@doofinder.com>
2020-09-23 13:34:43 +0200

    New Release: v1.8.33

--- app/code/community/Doofinder/Feed/controllers/DoofinderFeedLogController.php
+++ app/code/community/Doofinder/Feed/controllers/DoofinderFeedLogController.php
@@ -6,7 +6,7 @@
 /**
  * @category   controllers
  * @package    Doofinder_Feed
- * @version    1.8.32
+ * @version    1.8.33
  */
 
 class Doofinder_Feed_DoofinderFeedLogController extends Mage_Adminhtml_Controller_Action
